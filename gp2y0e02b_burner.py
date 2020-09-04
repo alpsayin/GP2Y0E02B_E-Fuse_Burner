1caeec844089f8b8662b6b13059a8792ec7b403d
Alp Sayin <alpsayin@gmail.com>
2020-09-04 11:10:10 +0300

    get rid of the unnecessary byte function

--- gp2y0e02b_burner.py
+++ gp2y0e02b_burner.py
@@ -25,9 +25,6 @@ ENABLE_VERIFICATION = False
 
 wire = None
 
-def byte(int_in):
-    return int_in & 0xFF
-
 class Wire(object):
     def __init__(self, dev=1):
         self.dev = dev
@@ -64,7 +61,7 @@ def EFuseSlaveID(newID):
     # ----- Stage 1 ----- */
     print("Stage 1 started.")
 
-    wire.write_sequence(byte(ADDRESS), byte(0xEC), byte(0xFF))
+    wire.write_sequence(ADDRESS, 0xEC, 0xFF)
     print("Data = 0xFF is set in Address = 0xEC")
 
     GPIO.output(VPP_PIN, GPIO.HIGH)
@@ -72,175 +69,175 @@ def EFuseSlaveID(newID):
 
     # ----- Stage 2 ------ */
     print("Stage 2 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xC8), byte(0x00))
+    wire.write_sequence(ADDRESS, 0xC8, 0x00)
     print("Data = 0x00 is set in Address = 0xC8")
 
     # ----- Stage 3 ------ */
     print("Stage 3 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xC9), byte(0x45))
+    wire.write_sequence(ADDRESS, 0xC9, 0x45)
     print("Data = 0x45 is set in Address = 0xC9")
 
     # ----- Stage 4 ------ */
     # THIS IS WHERE THE ADDRESS WILL BE SET! */
     print("Stage 4 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xCD), byte(newID >> 4))
+    wire.write_sequence(ADDRESS, 0xCD, newID >> 4)
     print("Data = SETADDR >> 4 is set in Address = 0xCD")
 
     # ----- Stage 5 ------ */
     print("Stage 5 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xCA), byte(0x01))
+    wire.write_sequence(ADDRESS, 0xCA, 0x01)
     print("Data = 0x01 is set in Address = 0xCA")
     print("Wait for 500 us")
     time.sleep(1e-6*500)
 
     # ----- Stage 6 ------ */
     print("Stage 6 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xCA), byte(0x00))
+    wire.write_sequence(ADDRESS, 0xCA, 0x00)
     print("Data = 0x00 is set in Address = 0xCA")
     GPIO.output(VPP_PIN, GPIO.LOW)
     print("Vpp terminal grounded.")
 
     # ----- Stage 7 ------ */
     print("Stage 7 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xEF), byte(0x00))
+    wire.write_sequence(ADDRESS, 0xEF, 0x00)
     print("Data = 0x00 is set in Address = 0xEF")
 
-    wire.write_sequence(byte(ADDRESS), byte(0xC8), byte(0x40))
+    wire.write_sequence(ADDRESS, 0xC8, 0x40)
     print("Data = 0x40 is set in Address = 0xC8")
 
-    wire.write_sequence(byte(ADDRESS), byte(0xC8), byte(0x00))
+    wire.write_sequence(ADDRESS, 0xC8, 0x00)
     print("Data = 0x00 is set in Address = 0xC8")
 
     # ----- Stage 8 ------ */
     print("Stage 8 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xEE), byte(0x06))
+    wire.write_sequence(ADDRESS, 0xEE, 0x06)
     print("Data = 0x06 is set in Address = 0xEE")
 
     # ----- Stage 9 ------ */
     print("Stage 9 started.")
-    wire.write_sequence(byte(ADDRESS), byte(0xEC), byte(0xFF))
+    wire.write_sequence(ADDRESS, 0xEC, 0xFF)
     print("Data = 0xFF is set in Address = 0xEC")
 
-    wire.write_sequence(byte(ADDRESS), byte(0xEF), byte(0x03))
+    wire.write_sequence(ADDRESS, 0xEF, 0x03)
     print("Data = 0x03 is set in Address = 0xEF")
 
     print("Read out the data in Address = 0x27.")
     print("Data: 0B")
-    x27Val = wire.read_sequence(byte(ADDRESS), byte(0x27))
+    x27Val = wire.read_sequence(ADDRESS, 0x27)
     print(f'{x27Val}')
 
-    wire.write_sequence(byte(ADDRESS), byte(0xEF), byte(0x00))
+    wire.write_sequence(ADDRESS, 0xEF, 0x00)
     print("Data = 0x00 is set in Address = 0xEF")
 
-    wire.write_sequence(byte(ADDRESS), byte(0xEC), byte(0x7F))
+    wire.write_sequence(ADDRESS, 0xEC, 0x7F)
     print("Data = 0x7F is set in Address = 0xEC")
 
     if ENABLE_VERIFICATION:
-        checkSum = wire.read_sequence(byte(ADDRESS), byte(0x27))
+        checkSum = wire.read_sequence(ADDRESS, 0x27)
         if (checkSum % 16) != 1:
             print("ERROR!   >:(\nE-fuse probably broken.")
             print("Data: 0B")
-            x27Val = wire.read_sequence(byte(ADDRESS), byte(0x27))
+            x27Val = wire.read_sequence(ADDRESS, 0x27)
             print(f'{x27Val}')
 
             # ----- Stage 10 - 1 ----- */
             print("Stage 10 - 1 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xEC), byte(0xFF))
+            wire.write_sequence(ADDRESS, 0xEC, 0xFF)
             print("Data = 0xFF is set in Address = 0xEC")
             GPIO.output(VPP_PIN, GPIO.HIGH)
             print("3.3V is applied in Vpp terminal")
 
             # ----- Stage 10 - 2 ----- */
             print("Stage 10 - 2 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xC8), byte(0x37))
+            wire.write_sequence(ADDRESS, 0xC8, 0x37)
             print("Data = 0x37 is set in Address = 0xC8")
 
             # ----- Stage 10 - 3 ----- */
             print("Stage 10 - 3 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xC9), byte(0x74))
+            wire.write_sequence(ADDRESS, 0xC9, 0x74)
             print("Data = 0x74 is set in Address = 0xC9")
 
             # ----- Stage 10 - 4 ----- */
             print("Stage 10 - 4 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xCD), byte(0x04))
+            wire.write_sequence(ADDRESS, 0xCD, 0x04)
             print("Data = 0x04 is set in Address = 0xCD")
 
             # ----- Stage 10 - 5 ----- */
             print("Stage 10 - 5 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xCA), byte(0x01))
+            wire.write_sequence(ADDRESS, 0xCA, 0x01)
             print("Data = 0x01 is set in Address = 0xCA")
             time.sleep(1e-6*500)
             print("Wait for 500 us.")
 
             # ----- Stage 10 - 6 ----- */
             print("Stage 10 - 6 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xCA), byte(0x00))
+            wire.write_sequence(ADDRESS, 0xCA, 0x00)
             print("Data = 0x00 is set in Address = 0xCA")
             GPIO.output(VPP_PIN, GPIO.LOW)
             print("Vpp terminal is grounded.")
 
             # ----- Stage 10 - 1' ----- */
             print("Stage 10 - 1' started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xEC), byte(0xFF))
+            wire.write_sequence(ADDRESS, 0xEC, 0xFF)
             print("Data = 0xFF is set in Address = 0xEC")
             GPIO.output(VPP_PIN, GPIO.HIGH)
             print("3.3V is applied in Cpp terminal")
 
             # ----- Stage 10 - 2' ----- */
             print("Stage 10 - 2' started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xC8), byte(0x3F))
+            wire.write_sequence(ADDRESS, 0xC8, 0x3F)
             print("Data = 0x3F is set in Address = 0xC8")
 
             # ----- Stage 10 - 3' ----- */
             print("Stage 10 - 3' started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xC9), byte(0x04))
+            wire.write_sequence(ADDRESS, 0xC9, 0x04)
             print("Data = 0x04 is set in Address = 0xC9")
 
             # ----- Stage 10 - 4' ----- */
             # THIS IS WHERE THE ADDRESS IS PROGRAMMED */
             print("Stage 10 - 4' started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xCD), byte(newID >> 4))
+            wire.write_sequence(ADDRESS, 0xCD, newID >> 4)
             print("Data = 0x08 is set in Address = 0xCD")
 
             # ----- Stage 10 - 5' ----- */
             print("Stage 10 - 5' started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xCA), byte(0x01))
+            wire.write_sequence(ADDRESS, 0xCA, 0x01)
             print("Data = 0x01 is set in Address = 0xCA")
             time.sleep(1e-6*500)
             print("Wait for 500 us.")
 
             # ----- Stage 10 - 6' ----- */
             print("Stage 10 - 6' started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xCA), byte(0x00))
+            wire.write_sequence(ADDRESS, 0xCA, 0x00)
             print("Data = 0x00 is set in Address = 0xCA")
             GPIO.output(VPP_PIN, GPIO.LOW)
             print("Vpp terminal is grounded.")
 
             # ----- Stage 10 - 7 ------ */
             print("Stage 10 - 7 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xEF), byte(0x00))
+            wire.write_sequence(ADDRESS, 0xEF, 0x00)
             print("Data = 0x00 is set in Address = 0xEF")
 
-            wire.write_sequence(byte(ADDRESS), byte(0xC8), byte(0x40))
+            wire.write_sequence(ADDRESS, 0xC8, 0x40)
             print("Data = 0x40 is set in Address = 0xC8")
 
-            wire.write_sequence(byte(ADDRESS), byte(0xC8), byte(0x00))
+            wire.write_sequence(ADDRESS, 0xC8, 0x00)
             print("Data = 0x00 is set in Address = 0xC8")
 
             # ----- Stage 10 - 8 ------ */
             print("Stage 10 - 8 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xEE), byte(0x06))
+            wire.write_sequence(ADDRESS, 0xEE, 0x06)
             print("Data = 0x06 is set in Address = 0xEE")
 
             # ------ Stage 10 - 9 ----- */
             print("Stage 10 - 9 started.")
-            wire.write_sequence(byte(ADDRESS), byte(0xEC), byte(0xFF))
+            wire.write_sequence(ADDRESS, 0xEC, 0xFF)
             print("Data = 0xFF is set in Address = 0xEC")
-            wire.write_sequence(byte(ADDRESS), byte(0xEF), byte(0x03))
+            wire.write_sequence(ADDRESS, 0xEF, 0x03)
             print("Data = 0x03 is set in Address = 0xEF")
 
-            x18Val = wire.read_sequence(byte(ADDRESS), byte(0x18))
-            x19Val = wire.read_sequence(byte(ADDRESS), byte(0x19))
+            x18Val = wire.read_sequence(ADDRESS, 0x18)
+            x19Val = wire.read_sequence(ADDRESS, 0x19)
 
             print(f"0x18 = {x18Val:0x}")
             print(f"\tx19Val = {x19Val:0x}")
